6e4031ed429533cd668bc81fb1aff22841afebf9
David CHENG <itsdc116@outlook.com>
2020-03-16 00:14:42 +0800

    for loop_cars/names

---
+++ for.py
@@ -0,0 +1,10 @@
+# for loop
+
+cars = ['Toyota', 'Honda', 'Audi', 'Benz']
+
+for car in cars: # 把清單中的東西一個一個拿出來 / 暫時稱呼
+	print(car) # 依照清單中的項目, 走幾次迴圈
+
+names = ['Allen', 'Tom', 'Jack']
+for name in names: 
+	print(name)
