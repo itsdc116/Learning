00238a6732edb0b8ec1ae221f594058b89654749
David CHENG <itsdc116@outlook.com>
2020-03-16 00:07:59 +0800

    list

---
+++ list.py
@@ -0,0 +1,19 @@
+# list 清單
+
+a = ['Toyota', 'Honda'] # 空清單
+print(a)
+
+# index(索引) (車廂代號, 從0開始)
+print(a[0])
+print(a[1])
+
+# .append() 加東西進清單
+a.append('Audi')
+print(a)
+
+# len() 取長度 length
+print(len(a))
+
+# in + 清單, 檢查東西有沒有在裡面
+print('Audi' in a) # 是非題 True, False
+print('Benz' in a)
